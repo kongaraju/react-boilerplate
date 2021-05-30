5dc2cc7afd6506405df37383759a97df757ccfd9
Konga Raju <raju.konga@ge.com>
2021-05-30 08:31:15 +0530

    Added material ui

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
